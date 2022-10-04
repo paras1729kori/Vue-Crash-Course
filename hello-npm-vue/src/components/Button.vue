42e947828d20eb79519404a62b23fa83ae55dd1d
paras1729kori <paras1799kori@gmail.com>
2022-10-04 17:29:13 +0530

    first commit

---
+++ hello-npm-vue/src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="onClick" :style="{ background: color }" className="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      comsole.log("click");
+    },
+  },
+};
+</script>
---
+++ hello-npm-vue/src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+  name: "Header",
+  // ways to setup props
+  //   props: ["title"], // 1
+  //   props: {          // 2
+  //     title: String
+  //   }
+  // 3rd way with default and specifiity
+  props: {
+    title: {
+      type: String,
+      default: "Header",
+    },
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
